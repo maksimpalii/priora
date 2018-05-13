177bf60861c0398f874345e7f75ddebe8099e2c4
Maksim Palii <maksim.palii@gmail.com>
2018-05-13 22:19:06 +0300

    13052018

---
+++ source/js/app.js
@@ -0,0 +1,66 @@
+"use strict";
+
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+};
+
+
+var preloader = (function () {
+    var percentsTotal = 0;
+    var preloader = $('.preloader');
+    var imgPath = $('*').map(function (ndx, element) {
+        var background = $(element).css('background-image');
+        var isImg = $(element).is('img');
+        var path = '';
+        if (background != 'none') {
+            path = background.replace('url("', '').replace('")', '');
+        }
+        if (isImg) {
+            path = $(element).attr('src');
+        }
+        if (path) return path;
+    });
+
+    var setPercents = function (total, current) {
+        var percents = Math.ceil(current / total * 100);
+
+        $('.loading-value').text(percents + '%');
+        $('.big.circle').css({'stroke-dasharray': percents * 1.57 + ' ' + '157'});
+
+        if (percents >= 100) {
+            preloader.fadeOut();
+        }
+    }
+
+    var loadImages = function (images) {
+
+        if (!images.length) preloader.fadeOut();
+
+        images.forEach(function (img, i, images) {
+            var fakeImage = $('<img>', {
+                attr: {
+                    src: img
+                }
+            });
+
+            fakeImage.on('load error', function () {
+                percentsTotal++;
+                setPercents(images.length, percentsTotal);
+            });
+        });
+
+    }
+    return {
+        init: function () {
+            var imgs = imgPath.toArray();
+            loadImages(imgs);
+            //console.log(imgs);
+        }
+    }
+}());
+
+preloader.init();
+
