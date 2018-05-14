6cf6594dda2e25535304191521e57c6eb194b6b8
Maksim Palii <maksim.palii@gmail.com>
2018-05-14 23:23:38 +0300

    14052018_vertical_menu_add

--- source/js/app.js
+++ source/js/app.js
@@ -111,7 +111,60 @@ $('html, body').animate({
     })
 });
 
+/* Menu vertical */
 
+var menuBlog = (function () {
+    var container = document.querySelector('.menu-blog-nav');
+    if (container !== null) {
+        var blogmenu = document.querySelector('.section-blog__menu');
+        var menuState = false;
+
+        container.addEventListener('click', function () {
+            if (!menuState) {
+                blogmenu.classList.add('active');
+                menuState = true;
+            }
+            else {
+                blogmenu.classList.remove('active');
+                menuState = false;
+            }
+        });
+    }
+});
+
+
+var blogscontent = document.querySelector('.section-blog__content');
+
+if (blogscontent){
+    var  menuOffsetTop = $('.section-blog__list').offset().top;
+    $(document).scroll(function () {
+        if ($(document).scrollTop() >= (menuOffsetTop - 100) && $(window).width() > 753){
+            $('.section-blog__list').addClass('fixed-position');
+        } else {
+            $('.section-blog__list').removeClass('fixed-position');
+        }
+
+
+        $(".section-blog__post").each(function () {
+            if (($(document).scrollTop() - $(this).offset().top) >= -150){
+                $(".section-blog__item").each(function () {
+                    $(this).removeClass('section-blog__item--active');
+
+                });
+                var currentLink = $(".articles__link[href=\'#" + $(this).attr('id') + "\']");
+                currentLink.parent().addClass('section-blog__item--active');
+            }
+        });
+    });
+
+    $(".articles__link").on("click", function (event) {
+        event.preventDefault();
+        var ids  = $(this).attr('href'),
+            topsb = $(ids).offset().top -100;
+        $('body,html').animate({scrollTop: topsb}, 1000);
+    });
+}
+menuBlog();
 
 
 
