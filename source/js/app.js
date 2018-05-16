bb640f7088996262319309f9c99a24824e214487
Maksim <maksim.palii@gmail.com>
2018-05-16 18:54:47 +0300

    16052018_planshet

--- source/js/app.js
+++ source/js/app.js
@@ -141,8 +141,8 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        height: '647',
-        width: '1150',
+        height: '100%',
+        width: '100%',
         videoId: '611Yy41Rubg',
         playerVars: { 'autoplay': 0, 'controls': 0 },
         events: {
